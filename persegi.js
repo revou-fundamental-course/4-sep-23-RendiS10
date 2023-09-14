e55cee11ce67962a193b9499a978d8cc0efc06c9
RendiS10 <rendisutendi10@gmail.com>
2023-09-14 15:43:31 +0700

    First Push

---
+++ persegi.js
@@ -0,0 +1,8 @@
+function luaspersegi() {
+  let hitung = document.getElementById("hitung").value;
+  alert("Hasil Perhitungan Luas Persegi adalah = " + hitung * hitung);
+}
+function kelilingpersegi() {
+  let hitung = document.getElementById("hitung").value;
+  alert("Hasil Perhitungan Keliling Persegi adalah = " + 4 * hitung);
+}
